11421966cb34a2b51aef21403d7f58b0e73fa5b2
happyjin0509 <102281315+happyjin0509@users.noreply.github.com>
2022-05-13 13:45:13 +0900

    update
    update

--- src/dao/BookstoreProvider.java
+++ src/dao/BookstoreProvider.java
@@ -1,5 +1,30 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class BookstoreProvider {
+
+	private static Connection conn;
+
+	private BookstoreProvider() {}
+
+	public static Connection getInstance() {
+		try {
+			if (conn == null) {
+
+				String url = "jdbc:oracle:thin:@localhost:1521:xe";
+				String user = "scott";
+				String password = "tiger";
+				conn = DriverManager.getConnection(url, user, password);
+//				System.out.println("DB connected !!");
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
 
 }
--- src/dto/Bookstore.java
+++ src/dto/Bookstore.java
@@ -2,4 +2,71 @@ package dto;
 
 public class Bookstore {
 
+	// field
+	private int bookNo;
+	private int price;
+	private int pubNo;
+	private String title;
+
+
+	public Bookstore() {
+	}
+
+	//constructor
+	public Bookstore(int bookNo, int price, int pubNo, String title) {
+		super();
+		this.bookNo = bookNo;
+		this.price = price;
+		this.pubNo = pubNo;
+		this.title = title;
+	}
+
+	//getter & setter
+	public int getBookNo() {
+		return bookNo;
+	}
+
+
+	public void setBookNo(int bookNo) {
+		this.bookNo = bookNo;
+	}
+
+
+	public int getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+
+	public int getPubNo() {
+		return pubNo;
+	}
+
+
+	public void setPubNo(int pubNo) {
+		this.pubNo = pubNo;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	//toString
+	@Override
+	public String toString() {
+		return "Bookstore [bookNo=" + bookNo + ", price=" + price + ", pubNo=" + pubNo + ", title=" + title + "]";
+	}
+
+
+	
 }
